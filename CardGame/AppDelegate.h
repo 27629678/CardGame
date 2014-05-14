083174596c3ee7750b6df153b83bab2d75371d20
H-YXH <sddz_yuxiaohua@163.com>
2014-05-14 10:02:24 +0800

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by H-YXH on 5/14/14.
+//  Copyright (c) 2014 NetEase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CardGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by H-YXH on 5/14/14.
+//  Copyright (c) 2014 NetEase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
